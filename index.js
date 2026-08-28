ae1430ed129b417763586cbe766efc5410320f8f
محمد عادل <  GitHub >
2026-08-28 11:26:20 +0300

    Use environment token

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const mineflayer = require('mineflayer');
 
@@ -109,4 +110,4 @@ client.on('messageCreate', (message) => {
     }
 });
 
-client.login('DISCORD_TOKEN');
+client.login(process.env.TOKEN);
